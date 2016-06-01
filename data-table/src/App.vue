fd42dd1b04a584df8bec1fda2f4d2cf3a2775637
YUyz <569812520@qq.com>
2016-06-01 17:17:28 +0800

    init data-table

--- data-table/src/App.vue
+++ data-table/src/App.vue
@@ -5,18 +5,11 @@
 </template>
 
 <script>
-import stock from './api/stock'
-
 export default {
   data () {
     return {
       msg: 'Hello World!'
     }
-  },
-  created () {
-    stock.get('aapl', (err, val) => {
-      console.log(err, val)
-    })
   }
 }
 </script>
